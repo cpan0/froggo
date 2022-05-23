5cf5f4d7860e3220c7fcd1fcb74781e2c91bc641
clare <f2l5o@ugrad.cs.ubc.ca>
2022-05-23 02:39:36 -0700

    wed frog :3

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const { Client, Intents, Guild, MessageAttachment } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const auth = require('./auth.json');
+var CronJob = require('cron').CronJob;
 
 client.on('ready', () => {
  console.log(`READY!!!`);
@@ -8,24 +9,24 @@ client.on('ready', () => {
 });
 
 client.on('ready', () => {
-    
-var CronJob = require('cron').CronJob;
 
- var job = new CronJob(
-     '01 * * * * *',
-     function() {
-         console.log('You will see this message every second');
+day_tasks('3', 'wed');
 
-         client.channels.cache.forEach(channel => {
-            if(channel.type === 'GUILD_TEXT') 
-            channel.send({ files: ['./images/3wed/' + Math.floor(Math.random() * 20) + '.jpg']});
-        })
+//  var job = new CronJob(
+//      '01 * * * * *',
+//      function() {
+//          console.log('You will see this message every second');
 
-        },
-    null,
-    true,
-    'America/Los_Angeles'
-    );
+//          client.channels.cache.forEach(channel => {
+//             if(channel.type === 'GUILD_TEXT') 
+//             channel.send({ files: ['./images/3wed/' + Math.floor(Math.random() * 20) + '.jpg']});
+//         })
+
+//         },
+//     null,
+//     true,
+//     'America/Los_Angeles'
+//     );
 
 });
 
@@ -36,3 +37,21 @@ client.on('message', msg => {
 });
 
 client.login(auth.token);
+
+function day_tasks(int, day) {
+    var job = new CronJob(
+        '* * * * * ' + int,
+        function() {
+            console.log('You will see this message every ' + day);
+   
+            client.channels.cache.forEach(channel => {
+               if(channel.type === 'GUILD_TEXT') 
+               channel.send({ files: ['./images/' + int + day + '/' + Math.floor(Math.random() * 20) + '.jpg']});
+           })
+   
+           },
+       null,
+       true,
+       'America/Los_Angeles'
+       );
+};

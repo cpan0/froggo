59a9ef49ef26d5f206025d256b181d2a830e2518
DRAGONROLL SUSHI <>
2022-05-26 04:12:41 +0000

    able to run on uptimebot

--- bot.js
+++ bot.js
@@ -1,57 +1,75 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Bot is now running!!'))
+
+app.listen(port, () =>
+  console.log(`Your app is listening a http://localhost:${port}`)
+);
+
+
+/// 
+
+
 const { Client, Intents, Guild, MessageAttachment } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const auth = require('./auth.json');
 var CronJob = require('cron').CronJob;
 
 client.on('ready', () => {
- console.log(`READY!!!`);
+  console.log(`READY!!!`);
 
 });
 
 client.on('ready', () => {
-
-day_tasks('3', 'wed');
-
-//  var job = new CronJob(
-//      '01 * * * * *',
-//      function() {
-//          console.log('You will see this message every second');
-
-//          client.channels.cache.forEach(channel => {
-//             if(channel.type === 'GUILD_TEXT') 
-//             channel.send({ files: ['./images/3wed/' + Math.floor(Math.random() * 20) + '.jpg']});
-//         })
-
-//         },
-//     null,
-//     true,
-//     'America/Los_Angeles'
-//     );
-
+  day_tasks('3', 'wed');
 });
 
 client.on('message', msg => {
- if (msg.content === 'hello') {
-  msg.channel.send('hi!!!!');
- }
+  if (msg.content === 'hello') {
+    msg.channel.send('hi!!!!');
+  } else if (msg.content.includes('hey')) {
+    msg.channel.send({files: ['./images/hi-hello-hey.jpeg']});
+  }
 });
 
-client.login(auth.token);
+client.on('message', msg => {
+  if (msg.content.includes('cool')) {
+    msg.channel.send('klar is so COOL i want to be as COOL as her! :3c');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'listen') {
+    msg.channel.send('im listening :3');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'f') {
+    msg.channel.send('f is for friends who do stuff together <3');
+  }
+});
 
 function day_tasks(int, day) {
-    var job = new CronJob(
-        '* * * * * ' + int,
-        function() {
-            console.log('You will see this message every ' + day);
-   
-            client.channels.cache.forEach(channel => {
-               if(channel.type === 'GUILD_TEXT') 
-               channel.send({ files: ['./images/' + int + day + '/' + Math.floor(Math.random() * 20) + '.jpg']});
-           })
-   
-           },
-       null,
-       true,
-       'America/Los_Angeles'
-       );
+  var job = new CronJob(
+    '0 0 0 * * ' + int,
+    function() {
+      console.log('You will see this message every ' + day);
+
+      client.channels.cache.forEach(channel => {
+        if (channel.type === 'GUILD_TEXT') {
+          channel.send({
+            files: ['./images/' + int + day + '/' +
+              Math.floor(Math.random() * 20) + '.jpg']
+          });
+        }
+      })
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+  );
 };
+
+client.login(process.env.TOKEN);
